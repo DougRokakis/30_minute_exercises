fce34a0dac6f0add9c58e13fceceda43d220345d
Douglas <Douglas@Jennifers-MacBook-Pro.local>
2021-04-20 15:59:02 -0400

    made small error, necessary correction

--- karvonen_heart_rate/karvonen_heart_rate.py
+++ karvonen_heart_rate/karvonen_heart_rate.py
@@ -4,7 +4,7 @@ hr_max = 220 - age
 resting_hr = 170 - resting_pulse
 hr_percentage = [.55, .60, .65, .70, .75, .80, .85, .90, .95]
 for i in hr_percentage:
-    print(round((resting_hr*i)+resting_pulse) + " bpm")
+    print(str(round((resting_hr*i)+resting_pulse)) + " bpm")
 print ("These are the correlating bpm's with a heart rate percentage of 55-95 accoring to your age and resting pulse.")
 print ("You should try and keep your maximum heart rate below " + str(hr_max) + " when exercising.")
 
