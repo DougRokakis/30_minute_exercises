65c944377ec78e5e3724b0eaffd0b73f90f615cb
Douglas <Douglas@Jennifers-MacBook-Pro.local>
2021-04-20 15:56:00 -0400

    I've completed the basic exercise but not in a tabular format. Really not 100% sure at this time how to do it.

---
+++ karvonen_heart_rate/karvonen_heart_rate.py
@@ -0,0 +1,12 @@
+age = int(input("Please enter your age: "))
+resting_pulse = int(input("Please enter your resting pulse: "))
+hr_max = 220 - age
+resting_hr = 170 - resting_pulse
+hr_percentage = [.55, .60, .65, .70, .75, .80, .85, .90, .95]
+for i in hr_percentage:
+    print(round((resting_hr*i)+resting_pulse) + " bpm")
+print ("These are the correlating bpm's with a heart rate percentage of 55-95 accoring to your age and resting pulse.")
+print ("You should try and keep your maximum heart rate below " + str(hr_max) + " when exercising.")
+
+#for i in hr_percentage:
+#    print (str(round(i*100)) +"%  |  " + str(karvonen_hr))
